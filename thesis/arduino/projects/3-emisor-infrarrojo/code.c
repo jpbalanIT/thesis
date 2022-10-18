ff7ab3402275e9486420ff59049b976f513716ee
JuanPabloBC7 <jpbalan@correo.url.edu.gt>
2022-10-17 21:55:52 -0600

    Mejorar documentación de ejemplo de infrarrojo de salida.

--- thesis/arduino/projects/3-emisor-infrarrojo/code.c
+++ thesis/arduino/projects/3-emisor-infrarrojo/code.c
@@ -14,26 +14,26 @@
 IRsend irsend;                              // Iniciar libreria para emitir señal
 const int myLED = 13;                       // LED
 int pinButton = 2;                          // Asignación de Pin 2 para el botón de encendido
-int valor_button;                           // Creación de valor del botón
+int valor;                                  // Creación de valor del botón
 
 void setup()
 {
   Serial.begin(9600);                       // Para verificar que se esta enviando la señal
   pinMode(pinButton,INPUT);                 // Uso de pin 2 como entrada
-  pinMode(myLED, OUTPUT);                   // initialize digital pin LED_BUILTIN as an output.
+  pinMode(myLED, OUTPUT);                   // Asignar un pin digital como salida (output)
 }
 
 void loop()
 {
-  valor_button = digitalRead(pinButton);    // Que valor tiene el pin 2
-  if(valor_button == HIGH)                  // Si esta arriba, entonces
+  valor = digitalRead(pinButton);           // Que valor tiene el pin 2
+  if(valor == HIGH)                         // Si esta arriba, entonces
   {
-    //digitalWrite(myLED, HIGH);              // turn the LED on (HIGH is the voltage level), Para verificar que si enciaenda una LED al presionar el botón
+    //digitalWrite(myLED, HIGH);              // Encender la LED de salida en pin 13, al presionar el botón
     //irsend.sendSony(0xA90, 12);             // Enviar señal para SONY pwr PIN 3 DE SALIDA PARA EL CODIGO HEXADECIMAL
     irsend.sendNEC(0x20DF10EF,32);          // Enviar señal para LG PIN 3 DE SALIDA PARA EL CODIGO HEXADECIMAL
     Serial.println("Enviado");              // Imprime por serial que fue enviado
     delay(100);                             // Delay de 100 ms
   } else {
-    digitalWrite(myLED, LOW);   // turn the LED on (HIGH is the voltage level)  
+    digitalWrite(myLED, LOW);               // Apagar led cuando el botón ya no esta presionado
   }
 }
